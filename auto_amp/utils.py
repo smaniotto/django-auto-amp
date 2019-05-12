ed4db9424237adea0824c9b6cfec7a5800cb3aaf
Bernardo Smaniotto <be.smaniotto@gmail.com>
2019-05-12 15:27:47 -0300

    feat(amp-base): add 'amp' attribute to the document html tag

--- auto_amp/utils.py
+++ auto_amp/utils.py
@@ -9,6 +9,9 @@ def add_amp_tags(content, path):
     """
     parsed_amp = parse_html(content)
 
+    parsed_amp = insert_html_amp(parsed_amp)
+    parsed_amp = insert_canonical_link(parsed_amp, path)
+
     return str(parsed_amp)
 
 
@@ -17,3 +20,20 @@ def parse_html(content):
     Parse a HTML document to a Python representation using BeautifulSoup.
     """
     return BeautifulSoup(content, "html.parser")
+
+
+def insert_html_amp(parsed_amp):
+    """
+    Inserts the 'amp' attribute to the document's 'html' tag.
+    """
+    parsed_amp.html["amp"] = ""
+    return parsed_amp
+
+
+def insert_canonical_link(parsed_amp, path):
+    """
+    Inserts a link to the respetive canonical page.
+    """
+    canonical_link = parsed_amp.new_tag("link", rel="canonical", href=path)
+    parsed_amp.head.append(canonical_link)
+    return parsed_amp
--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,29 @@ from auto_amp import utils
 from test_utils import reload_module, reload_urlconf
 
 
+@pytest.fixture
+def parsed_html():
+    """
+    Fixture to return a valid and clean HTML document to apply AMP updates individually.
+    """
+    return utils.parse_html(
+        """
+        <!doctype hmtl>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Page title</title>
+            </head>
+            <body>
+                <h1>Django Auto AMP</h1>
+                <p>Generate automatic AMP from your Django templates</p>
+            </body>
+        </html>
+        """
+    )
+
+
 def test_canonical_to_amp_path_discovery(client, mocker):
     """
     Asserts that the AMP path directs the user to the correct respective canonical
@@ -23,3 +46,20 @@ def test_canonical_to_amp_path_discovery(client, mocker):
     assert amp_response.status_code == 200
     assert mocked_add_amp_tags.call_count == 1
     assert mocked_website_index.call_count == 1
+
+
+def test_insert_html_amp(parsed_html):
+    """
+    Asserts that the 'amp' attribute is added to a valid and clean HTML document.
+    """
+    parsed_amp = utils.insert_html_amp(parsed_html)
+    assert parsed_amp.find("html", amp="") is not None
+
+
+def test_insert_canonical_link(parsed_html):
+    """
+    Asserts that a link to the canonical path is added to the HTML header.
+    """
+    path = "/index"
+    parsed_amp = utils.insert_canonical_link(parsed_html, path)
+    assert parsed_amp.head.find("link", rel="canonical", href=path) is not None
